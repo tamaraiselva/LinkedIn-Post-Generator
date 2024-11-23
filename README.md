3ea16c444ad732014522bcc7a4101dfc2374da4c
tamaraiselva <tamaraiselvan98@gmail.com>
2024-11-23 17:06:19 +0530

    project file add

---
+++ README.md
@@ -0,0 +1,30 @@
+# project-genai-post-generator
+
+This tool will analyze posts of a LinkedIn influencer and help them create the new posts based on the writing style in their old posts
+
+[source](resources/tool.jpg)
+
+Let's say Mohan is a LinkedIn influencer and he needs help in writing his future posts. He can feed his past LinkedIn posts to this tool and it will extract key topics. Then he can select the topic, length, language etc. and use Generate button to create a new post that will match his writing style.
+
+## Technical Architecture
+
+[source](resources/architecture.jpg)
+
+Stage 1: Collect LinkedIn posts and extract Topic, Language, Length etc. from it.
+Stage 2: Now use topic, language and length to generate a new post. Some of the past posts related to that specific topic, language and length will be used for few shot learning to guide the LLM about the writing style etc.
+
+## Set-up
+
+1. To get started we first need to get an API_KEY from here: [API_KEY](https://console.groq.com/keys). Inside `.env` update the value of `GROQ_API_KEY` with the API_KEY you created.
+
+2. To get started, first install the dependencies using:
+
+```commandline
+     pip install -r requirements.txt
+```
+
+3. Run the streamlit app:
+
+```commandline
+   streamlit run main.py
+```
---
+++ few_shot.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import json
+
+class FewShotPosts:
+    def __init__(self, file_path="data/processed_posts.json"): 
+        self.df = None
+        self.unique_tags = None
+        self.load_posts(file_path)
+
+    def load_posts(self, file_path):
+        with open(file_path, encoding="utf-8") as f:
+            posts = json.load(f)
+            self.df = pd.json_normalize(posts)
+            self.df['length'] = self.df['line_count'].apply(self.categorize_length)
+            # collect unique tags
+            all_tags = self.df['tags'].apply(lambda x: x).sum()
+            self.unique_tags = list(set(all_tags))
+
+    def get_filtered_posts(self, length, language, tag):
+        df_filtered = self.df[
+            (self.df['tags'].apply(lambda tags: tag in tags)) &  # Tags contain the specified tag
+            (self.df['language'] == language) &  # Language matches the specified language
+            (self.df['length'] == length)  # Length matches the specified length category
+        ]
+        return df_filtered.to_dict(orient='records')
+
+    def categorize_length(self, line_count):
+        if line_count < 5:
+            return "Short"
+        elif 5 <= line_count <= 10:
+            return "Medium"
+        else:
+            return "Long"
+
+    def get_tags(self):
+        return self.unique_tags
+
+if __name__ == "__main__":
+    fs = FewShotPosts()
+    # Example usage
+    posts = fs.get_filtered_posts("Medium", "Hinglish", "Job Search")
+    print(posts)
